bd1f0aac90100e8634033e59b475e3463bacef03
John Clements <ClementsJR@users.noreply.github.com>
2022-07-16 23:17:44 -0400

    Added direct rotation controls to the dice.

---
+++ Assets/Scripts/DieRotator.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DieRotator : MonoBehaviour {
+
+	[SerializeField]
+	private Rigidbody rigidBody;
+	[SerializeField]
+	private float force = 0.25f;
+
+	private void FixedUpdate() {
+		float deltaPitch = Input.GetAxis("Vertical") * force;
+		float deltaRoll = -Input.GetAxis("Horizontal") * force;
+
+		Vector3 rotation = new Vector3(deltaPitch, 0.0f, deltaRoll);
+
+		rigidBody.AddTorque(rotation);
+	}
+}
